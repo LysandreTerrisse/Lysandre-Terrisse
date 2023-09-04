a19d1c7f6f1a00cc7e246a167084daa63f0196b5
LysandreTerrisse <124552354+LysandreTerrisse@users.noreply.github.com>
2023-09-04 17:59:07 +0200

    Update main.py
    - Uses cross-entropy cost function instead of quadratic cost function.
    - Divided the input layer by 255 to get a value between 0 and 1
    - Uses a Gaussian distribution of mean 0 and variance 1/sqrt(length of previous layer) (instead of mean 0 and variance 1)
    - Uses 400 hidden neurons instead of 30
    - Reaches 98.4% and then decreases to 98.3% (instead of converging just above 90%)

--- Neural Network/main.py
+++ Neural Network/main.py
@@ -5,8 +5,8 @@ def get_data(path_images, path_labels, n_elements):
 	with gzip.open(path_images, "rb") as fd1, gzip.open(path_labels, "rb") as fd2:
 		fd1.read(16)
 		fd2.read(8)
-		images = [np.array(list(fd1.read(28*28)))/255 for _ in range(n_elements)]
-		labels = [np.arange(10)==label for label in list(fd2.read(n_elements))]
+		images = np.split(np.array(list(fd1.read()))/255, n_elements)
+		labels = [np.arange(10)==label for label in list(fd2.read())]
 		return list(zip(images, labels))
 
 def sigmoid(x):
@@ -15,7 +15,7 @@ def sigmoid(x):
 class Neural_network:
 	def __init__(self, shape, is_random=False):
 		self.shape = shape
-		self.W = np.array([np.random.randn(y, x) if is_random else np.empty([y, x]) for x, y in zip([0] + shape[:-1], [0] + shape[1:])], dtype=object)
+		self.W = np.array([np.random.randn(y, x)/np.sqrt(x) if is_random else np.empty([y, x]) for x, y in zip([0] + shape[:-1], [0] + shape[1:])], dtype=object)
 		self.b = np.array([np.random.randn(size_layer) if is_random else np.empty(size_layer) for size_layer in [0] + shape[1:]], dtype=object)
 		self.a = np.array([np.empty(size_layer) for size_layer in shape], dtype=object)
 	
@@ -32,7 +32,7 @@ class Neural_network:
 		gradient = Neural_network(self.shape)
 		
 		for i in range(len(self.shape)-1, 0, -1):
-			gradient.a[i] = 2 * (a[-1] - y) if i==len(self.shape)-1 else np.dot(W[i+1].T, gradient.b[i+1])
+			gradient.a[i] = np.nan_to_num((a[i] - y) / (a[i] * (1 - a[i])), posinf=0, neginf=0) if i==len(self.shape)-1 else np.dot(W[i+1].T, gradient.b[i+1])
 			gradient.b[i] = gradient.a[i] * a[i] * (1 - a[i])
 			gradient.W[i] = np.outer(gradient.b[i], a[i-1])
 			
@@ -63,4 +63,4 @@ def learn(training_data, test_data, shape, learning_rate, minibatch_size):
 
 training_data = get_data("train-images-idx3-ubyte.gz", "train-labels-idx1-ubyte.gz", 60000)
 test_data = get_data("t10k-images-idx3-ubyte.gz", "t10k-labels-idx1-ubyte.gz", 10000)
-learn(training_data, test_data, [28*28, 30, 10], 1.5, 10)
+learn(training_data, test_data, [28*28, 400, 10], 0.5, 10)
